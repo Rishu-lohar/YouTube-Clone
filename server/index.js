399d3c525f9013c47d965457ea5ec1791eb397b0
Rishu Lohar <rishulohar266@gmail.com>
2026-09-25 14:09:02 +0530

    Fix production CORS

--- server/index.js
+++ server/index.js
@@ -35,7 +35,12 @@ const io = new Server(server, {
   },
 });
 
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ extended: true, limit: "30mb" }));
